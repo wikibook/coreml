31e036a21ee64705d33530f5cbcca43e58ae0f60
위키북스 <dylee@wikibook.co.kr>
2019-04-18 15:52:26 +0900

    Add files via upload

---
+++ Chapter4/Start/ExploringExpressionRecognition.playground/Sources/ImagePropertyOrientationExtension.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+/**
+ Extension to CGImagePropertyOrientation and UIImageOrientation for interoperability.
+ https://developer.apple.com/documentation/imageio/cgimagepropertyorientation
+ */
+
+public extension CGImagePropertyOrientation {
+    public init(_ uiOrientation: UIImage.Orientation) {
+        switch uiOrientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        }
+    }
+}
+
+public extension UIImage.Orientation {
+    public init(_ cgOrientation: UIImage.Orientation) {
+        switch cgOrientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        }
+    }
+}
